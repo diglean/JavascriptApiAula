3e3ce6447cc4dbf33f8a03584e48d544c57711fc
Diglean <90386523+diglean@users.noreply.github.com>
2022-04-26 20:14:18 -0300

    Add files via upload
    Adicionando detalhes no consumo de API's

--- 2022.04.25/js/main.js
+++ 2022.04.25/js/main.js
@@ -8,10 +8,41 @@ function GetClima()
 
         success: function(data){
             console.log(data)
+            plotarResultados(data);
+            localStorage.clima = JSON.stringify(data);
+            localStorage.alteracaoClima = new Date().getTime();
         },
 
-        error: function(argument){
+        error: function(argument)
+        {
 
         }
     });
+}
+
+function plotarResultados(clima){
+    let humidade = document.getElementById("humidade");
+    let temperaturaMax = document.getElementById("temperaturaMax");
+    let temperaturaMin = document.getElementById("temperaturaMin");
+    humidade.innerHTML = clima.main.humidity;
+    temperaturaMax.innerHTML = clima.main.temp_max;
+    temperaturaMin.innerHTML = clima.main.temp_min;
+}
+
+function obterDados(){
+    let tempoAtual = new Date().getTime() / 1000;
+    let tempoCache = parseInt(localStorage.alteracaoClima) / 1000;
+    if(tempoAtual - tempoCache >= (5*60))
+    {
+        GetClima();
+    }
+    else
+    {
+        plotarResultados(JSON.parse(localStorage.clima));
+    }
+
+}
+
+window.onload = () => {
+    obterDados();
 }
